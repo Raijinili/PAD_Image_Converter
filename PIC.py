038df1af4e25d248a934003fd2a1d2576977c802
Raijinili <Raijinili@users.noreply.github.com>
2016-01-24 07:15:14 -0500

    Just some style things
    - Consistently use spaces instead of tabs.
    - Use `glob.glob` to grab the file list.
    - Reading in binary mode makes it return ints (automatic `ord`) instead of characters.
    - Read the header off all at once.
    - Use `0x` hex literals.
    - Enumerate instead of manually keeping indices.
    - Group pairs of bytes in the image instead of grouping at the end.

--- PIC.py
+++ PIC.py
@@ -1,44 +1,44 @@
 import os
+import glob
 from PIL import Image
 
 
 def group(lst, n):
-	for i in range(0, len(lst), n):
-		val = lst[i:i+n]
-		if len(val) == n:
-			yield tuple(val)
+    for i in range(0, len(lst), n):
+        val = lst[i:i+n]
+        if len(val) == n:
+            yield tuple(val)
 
-fileList = []			
-for filename in os.listdir("."):    
-    if filename.endswith('.bc'):
-        fileList.append(filename)
 
-for currentFile in fileList:
-    x = 0
-    numBytes = 0
-    out = []
-    with open(currentFile) as f:
-        byte = f.read(1)
-        while byte != "":
-            x += 1
-            # Do stuff with byte.
-            if x == 22:
-                sizeData = '{:02x}'.format(ord(byte))
-                if sizeData == '32':
-                    picSize = 512
-                elif sizeData == '31':
-                    picSize = 256
-                else:
-                    picSize = 128
-                numBytes = picSize*picSize*2+48    
+for currentFile in glob.iglob('./*.bc'):
+    with open(currentFile, 'rb') as f:
         
-            if x > 49 and x < numBytes:
-                d = '{:02x}'.format(ord(byte))
-                out.append(int("0x%s%s" % (d[0],d[0]), 16))
-                out.append(int("0x%s%s" % (d[1],d[1]), 16))
-            byte = f.read(1)
-			
+        # byte = f.read(1)
+        header = f.read(48)
+        
+        sizeData = header[21]
+        
+        if sizeData == 0x32:
+            picSize = 512
+        elif sizeData == 0x31:
+            picSize = 256
+        else:
+            picSize = 128
+        
+        numBytes = picSize*picSize*2
+        
+        # Should be small enough to fit in memory.
+        image = f.read(numBytes)
+        
+    out = []
+    for rg, ba in group(image, 2):
+        r, g = divmod(byte, 16)
+        b, a = divmod(byte, 16)
+        
+        #!TODO: Check order.
+        out.append((r, g, b, a))
+    
     newimage = Image.new('RGBA', (picSize, picSize))  # type, size
-    newimage.putdata(list(group(out, 4)))
-    pngName = currentFile[:len(currentFile)-2] + "png"
+    newimage.putdata(out)
+    pngName = currentFile[:-2] + "png"
     newimage.save(pngName)
